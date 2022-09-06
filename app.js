b88fadd7de98f800759f543e65a5d11eb255a104
joel casco <joel_casco@hotmail.com.ar>
2022-09-05 23:28:32 -0300

    segunda entraga

---
+++ app.js
@@ -0,0 +1,45 @@
+//DOM interactucar con HTML recibimos con getElement y mandamos a p con .innerHTML
+function calcDolar(){
+    let num1 = document.getElementById("num1").value;
+    document.getElementById("rtaDlr").innerHTML = num1 * 144,86;
+    localStorage.setItem('num1', num1)
+}
+
+function calcBlue(){
+    let num2 = document.getElementById("num2").value;
+    document.getElementById("rtaBlue").innerHTML = num2 * 200;
+}
+
+function calcEuro(){
+    let num3 = document.getElementById("num3").value;
+    document.getElementById("rtaEur").innerHTML = num3 * 137,08;
+}
+
+//Clase metodo 2 .on / Evento onclick
+let botonDlr = document.getElementById("calcDlr")
+botonDlr.addEventListener("click", calcDolar);
+
+
+let botonBlue = document.getElementById("calcBlue")
+botonBlue.addEventListener("click", calcBlue);
+
+let botonEur = document.getElementById("calcEur")
+botonEur.addEventListener("click", calcEuro);
+
+
+const formulario = document.querySelector("#formulario");
+console.log(formulario)
+//crear el evento para que escuche el submit y nombro la funcion
+
+formulario.addEventListener("submit", enviarFormulario);
+
+function enviarFormulario (e){
+    e.preventDefault()
+    const user = document.querySelector("#usuario").value
+    const edad = document.querySelector("#clave").value
+    const respuesta = document.getElementById("respuesta");
+    respuesta.textContent = `HOLA${user} tienes la edad de ${edad}`
+    localStorage.setItem('user', user)
+    localStorage.setItem('edad', edad)
+}
+enviarFormulario()
---
+++ main.js
@@ -0,0 +1,50 @@
+//declaramos las variables
+// let formulario = document.getElementById("login");
+// let resultado = document.querySelector('#error');
+// //creo una class para obtener los datos de los usuarios
+// class Usuario {
+//     constructor(user, password){
+//         this.user = user;
+//         this.password = password;
+//     }
+    
+// }
+// //creo un array para almacenar los usuarios
+// let usuarios = [];
+// let usuario = new Usuario("Joel", "2341");
+// let usuario2 = new Usuario("Santiago", "1234");
+
+// usuarios.push(usuario,usuario2);
+
+// // declaro un evento para agregar una funcion para enviar el form
+// formulario.addEventListener("submit", (e) => {
+//     e.preventDefault(); // prevenimos el evento para que no se mande directament el form
+//     let usuarioLogin = document.getElementById("user").value;
+//     let passLogin = document.getElementById("pass").value;
+
+//     for(let user of usuarios){
+//         if (user.user == usuarioLogin && user.password == passLogin){
+//             alert("acceso correcto");
+//             window.location.href = "welcome.html" // para que me redireccione al html de la calculadora
+//         } else  if ((user.user =! usuarioLogin) && (user.password =! passLogin) || (user.user =! usuarioLogin) ||  (user.password =! passLogin) ){
+//             resultado.innerHTML = `<p> acceso incorrecto, revise sus datos</p>`
+//         }
+//     }
+// })
+// console.log(formulario)
+
+let boton = document.getElementById("entrar");
+let resultado = document.querySelector('#error');
+boton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let usuario = document.getElementById("usuario").value;
+  let pass = document.getElementById("pass").value;
+
+  if (usuario == "Jose" && pass == "1234") {
+    window.location.href = "welcome.html";
+    localStorage.setItem("nombre", usuario)
+  } else {
+    resultado.innerHTML = `<p> acceso incorrecto, revise sus datos</p>`
+     
+  }
+})
